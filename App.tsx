02d6a5b5e18bd4d9f2588422e611ff3c0d8e7518
Jhon9191 <joaoantonio15161516@gmail.com>
2021-06-23 22:52:45 -0300

    Configuring navigations

--- App.tsx
+++ App.tsx
@@ -2,41 +2,42 @@ import React from 'react';
 
 import { StatusBar } from 'react-native';
 import { useFonts } from 'expo-font';
-import { 
-Inter_400Regular,
-Inter_500Medium } 
-from '@expo-google-fonts/inter';
-import { 
-Rajdhani_500Medium, 
-Rajdhani_700Bold } 
-from '@expo-google-fonts/rajdhani';
+import {
+  Inter_400Regular,
+  Inter_500Medium
+}
+  from '@expo-google-fonts/inter';
+import {
+  Rajdhani_500Medium,
+  Rajdhani_700Bold
+}
+  from '@expo-google-fonts/rajdhani';
 import AppLoading from 'expo-app-loading';
 import { Background } from './src/components/Background';
 
-import { SignIn } from './src/screens/SignIn';
-import { Home } from './src/screens/Home';
+import { Routes } from './src/routes';
 
 export default function App() {
 
   const [fontsLoaded] = useFonts({
-    Inter_400Regular, 
+    Inter_400Regular,
     Inter_500Medium,
-    Rajdhani_500Medium, 
+    Rajdhani_500Medium,
     Rajdhani_700Bold
   });
 
-  if(!fontsLoaded){
-    return <AppLoading/>
+  if (!fontsLoaded) {
+    return <AppLoading />
   }
 
   return (
     <Background>
-    <StatusBar barStyle="light-content"
-    backgroundColor="transparent"
-    translucent
-    hidden={true}
-    />
-    <Home/>
+      <StatusBar barStyle="light-content"
+        backgroundColor="transparent"
+        translucent
+        hidden={true}
+      />
+      <Routes />
     </Background>
   );
 }
--- src/screens/SignIn/index.tsx
+++ src/screens/SignIn/index.tsx
@@ -1,36 +1,36 @@
 import React from 'react';
 import { Text, View, Image, StatusBar } from 'react-native';
 
-
 import ilustration from '../../assets/illustration.png';
 import { style } from './styles';
 import ButtonIcon from '../../components/ButtonIcon';
 
 export function SignIn() {
+
     return (
-    <View style={style.container}>
+        <View style={style.container}>
 
-        <Image source={ilustration}
-        style={style.image}
-        resizeMode="stretch"/>
+            <Image source={ilustration}
+                style={style.image}
+                resizeMode="stretch" />
 
-        <View style={style.content}>
-            <Text style={style.title}>
-                Conecte-se{`\n`}
-                e organize {`\n`}
-                suas jogatinas
-            </Text>
+            <View style={style.content}>
+                <Text style={style.title}>
+                    Conecte-se{`\n`}
+                    e organize {`\n`}
+                    suas jogatinas
+                </Text>
 
-            <Text style={style.subtitle}>
-                Crie grupos para jogar seus games {`\n`}
-                favoritos com seus amigos
-            </Text>
+                <Text style={style.subtitle}>
+                    Crie grupos para jogar seus games {`\n`}
+                    favoritos com seus amigos
+                </Text>
 
-        <ButtonIcon title="Entrar com Discord"
-        activeOpacity={0.7}/>
+                <ButtonIcon title="Entrar com Discord"
+                    activeOpacity={0.7} />
+            </View>
+
+
         </View>
-
-
-    </View>
-  )
+    )
 }

bc21ada7c4c4780197ad1efd4fbf397806db0672
Jhon9191 <joaoantonio15161516@gmail.com>
2021-07-05 19:27:12 -0300

    Creating members component

--- src/screens/AppointmentDetails/index.tsx
+++ src/screens/AppointmentDetails/index.tsx
@@ -1,27 +1,78 @@
 import React from 'react';
-import { View } from 'react-native';
+import {
+  ImageBackground,
+  Text,
+  View, 
+  FlatList
+} from 'react-native';
 import { BorderlessButton } from 'react-native-gesture-handler';
-import { style } from './styles';
+import { styles } from './styles';
 import { Background } from '../../components/Background';
+import { ListDivider } from '../../components/ListDivider';
+import { Member } from '../../components/Member';
 import { Header } from '../../components/Header';
 import { Fontisto } from '@expo/vector-icons';
 import { theme } from '../../global/styles/theme';
-export function AppointmentDetails(){
+import Banner from '../../assets/banner.png'
+import { ListHeader } from '../../components/ListHeader';
+export function AppointmentDetails() {
+
+  const members = [
+    {
+      id: "1",
+      username: "Joao",
+      avatar_url: "https://github.com/Jhon9191.png"
+    },
+    {
+      id: "2",
+      username: "Joao",
+      avatar_url: "https://github.com/Jhon9191.png"
+    },
+  ]
+
   return (
-      <Background>
-        <Header
-          title="Detalhes"
-          action={
-            <BorderlessButton>
-              <Fontisto
-                name="share"
-                size={24}
-                color={theme.colors.primary}
-              />
-            </BorderlessButton>
-          }
+    <Background>
+      <Header
+        title="Detalhes"
+        action={
+          <BorderlessButton>
+            <Fontisto
+              name="share"
+              size={24}
+              color={theme.colors.primary}
+            />
+          </BorderlessButton>
+        }
+      />
+      <ImageBackground
+        style={styles.banner}
+        source={Banner}>
+        <View style={styles.bannerContent}>
+          <Text style={styles.title}>
+            Lendários
+          </Text>
+          <Text style={styles.subtitle}>
+            É hoje que vamos chegar ao challenger sem perder uma partida da md10
+          </Text>
+        </View>
+      </ImageBackground>
+
+        <ListHeader
+          title="Jogadores"
+          subtitle="Total 3"
         />
-      </Background>
+
+        <FlatList
+          data={members}
+          keyExtractor={item => item.id}
+          renderItem={({item})=>(
+            <Member data={item}/>
+          )}
+          ItemSeparatorComponent={()=><ListDivider/>}
+          style={styles.members}
+        />
+
+    </Background>
   )
 }
 
--- src/screens/AppointmentDetails/styles.ts
+++ src/screens/AppointmentDetails/styles.ts
@@ -1,13 +1,44 @@
 import { StyleSheet } from 'react-native';
 import { theme } from '../../global/styles/theme';
 
-export const style = StyleSheet.create({
+export const styles = StyleSheet.create({
 
   container: {
     flex: 1,
     justifyContent: 'center',
     alignItems: 'center',
     //backgroundColor: theme.colors.secondary90
+  },
+
+  banner: {
+    width: '100%',
+    height: 234,
+    marginBottom: 30
+  },
+
+  bannerContent: {
+    flex: 1,
+    justifyContent: 'flex-end',
+    paddingHorizontal: 24,
+    marginBottom: 30
+  },
+
+  title: {
+    fontSize: 28,
+    fontFamily: theme.fonts.title700,
+    color: theme.colors.heading
+  },
+  
+  subtitle: {
+    fontSize: 13,
+    fontFamily: theme.fonts.text400,
+    color: theme.colors.heading,
+    lineHeight: 21
+  },
+
+  members: {
+    marginLeft: 24,
+    marginTop: 27
   },
 
 });

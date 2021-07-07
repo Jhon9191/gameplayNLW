cb2d13ae0fcd07fe7ecff52c2df337f2f74d880c
Jhon9191 <joaoantonio15161516@gmail.com>
2021-07-07 00:48:15 -0300

    Displaying logged user information

--- src/components/Profile/index.tsx
+++ src/components/Profile/index.tsx
@@ -6,16 +6,18 @@ Image,
 Text
 } from 'react-native';
 import {Photo} from '../../components/Photo';
+import { useAuth } from '../../hooks/auth';
 export function Profile(){
+  const { user } = useAuth()
   return(
     <View style={styles.container}>
 
-      <Photo urlImage="https://github.com/Jhon9191.png"/>
+      <Photo urlImage={user.avatar}/>
 
     <View>
       <View style={styles.user}> 
         <Text style={styles.greeting}>Olá,</Text>
-        <Text style={styles.username}>João</Text>
+        <Text style={styles.username}>{user.firstName}</Text>
       </View>
       <Text style={styles.message}>Hoje é dia de vitória</Text>
     </View>
